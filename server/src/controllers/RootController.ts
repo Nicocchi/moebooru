88b2ee51ea76975ec7941ed079a4da722a1a95ed
Nicocchi <nicocchilover@gmail.com>
2023-03-09 13:41:23 -0500

    added root route and updated tsconfig outdir

---
+++ server/src/controllers/RootController.ts
@@ -0,0 +1,22 @@
+import * as express from "express";
+import { CompareSync, GenerateAccessToken, GenerateRefreshToken } from "utils";
+
+class RootController {
+  public path = "/";
+  public router = express.Router();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  public initializeRoutes() {
+    this.router.post(this.path, this.root);
+  }
+
+  root = async (_req: express.Request, res: express.Response) => {
+    
+    return res.status(200).send("Hi");
+  };
+}
+
+export default RootController;
--- server/src/index.ts
+++ server/src/index.ts
@@ -9,11 +9,13 @@ import ArtistsController from "controllers/ArtistsController";
 import RefreshTokenController from "controllers/RefreshTokenController";
 import LogoutController from "controllers/LogoutController";
 import RolesController from "./controllers/RolesController";
+import RootController from "./controllers/RootController";
 
 const _PORT = Number(process.env.PORT) || 5000;
 
 const app = new App(
   [
+    new RootController(),
     new LoginController(),
     new LogoutController(),
     new RegisterController(),
